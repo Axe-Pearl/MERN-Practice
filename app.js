c73e9efbdfaa0caa35d7dc7f64b8dcd4aa98ab1c
Axe-Pearl <mycoursesbattletour@gmail.com>
2022-02-03 00:21:48 +0530

    added post method

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 const students = [
     {
         id: 1,
@@ -30,9 +35,14 @@ app.get("/student/:id",(req,res)=>{
     const stu_id = req.params.id;
     const data = students[stu_id-1].name;
     res.send(data);
-})
+});
 
-
+app.post("/add-student",(req,res)=>{
+     const tempData = req.body;
+     students.push(tempData);
+     console.log(students);
+     res.send("Data saved sucessfully!");
+});
 
 app.listen(3000,(req,res)=>{
     console.log("Listening port on 3000, Jai Shree Ram Jai Shree Hanuman Om Namah Shivaya");
